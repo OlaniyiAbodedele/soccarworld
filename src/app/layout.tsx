3bcd5440e71c5482f8f285d026708e0c477a6f10
Olaniyi Abodedele <founder@soccarworld.com>
2026-08-17 14:01:31 +0200

    Enable Analytics Debug Mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -83,7 +83,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
-        <Analytics />
+        <Analytics debug />
       </body>
     </html>
   );
